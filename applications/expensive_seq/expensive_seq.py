d1a5e4c53a46a59363ffaa80b3ed0222d4c134e6
Jasmine Terry <jasmine-terry91@gmail.com>
2020-06-07 17:17:40 -0400

    expensive pass

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,15 @@
-# Your code here
+
 dict = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-    
+
+    if x <= 0:
+        return y +z
+    if x >0:
+        if (x,y,z) not in dict:
+            dict[(x,y,z)] = expensive_seq(x-1, y+1, z) + \
+                expensive_seq(x-2, y+2,z*2) + expensive_seq(x-3, y+3, z*3)
+        return dict[(x, y,z)]
 
 
 
